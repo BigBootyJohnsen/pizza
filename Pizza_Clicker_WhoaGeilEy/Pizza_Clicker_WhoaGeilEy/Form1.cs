ff1feed8028ae554eafde21453740a3c5f920b75
moritz.albach@t-online.de <moritz.albach@t-online.de>
2020-12-23 16:54:20 +0100

    Änderung von int zu long und entfernen der punkte bei den zahlen

--- Pizza_Clicker_WhoaGeilEy/Pizza_Clicker_WhoaGeilEy/Form1.cs
+++ Pizza_Clicker_WhoaGeilEy/Pizza_Clicker_WhoaGeilEy/Form1.cs
@@ -13,28 +13,31 @@ namespace Pizza_Clicker_WhoaGeilEy
 {
     public partial class Form1 : Form
     {
-        public int pizza_count = 0; // Gibt an wie viele Pizzen wir haben.
-        public int pizza_click = 1; // Gibt an wie viele Pizzen ein Click auf den Button gibt.
-        public int pizza_cpt = 1; // Gibt an wie viele Clicks man pro Sekunde bekommt. (Autoclicks) (clicks per time)    
+        public long pizza_count = 0; // Gibt an wie viele Pizzen wir haben.
+        public long pizza_click = 1; // Gibt an wie viele Pizzen ein Click auf den Button gibt.
+        public long pizza_cpt = 1; // Gibt an wie viele Clicks man pro Sekunde bekommt. (Autoclicks) (clicks per time)    
 
-        public int pizza_up1_cost = 15;
-        public int pizza_up2_cost = 100;
-        public int pizza_up3_cost = 1100;
-        public int pizza_up4_cost = 12000;
-        public int pizza_up5_cost = 130000;
-        public int pizza_up6_cost = 1400000;
-        public int pizza_up7_cost = 20000000;
-        public int pizza_up8_cost = 330.000.000;
-        public int pizza_up9_cost = 5.100.000.00;
-        public int pizza_up10_cost = 75.000.000.000;
-        public int pizza_up11_cost = 1.000.000.000.000;
-        public int pizza_up12_cost = 14.000.000.000.000;
-        public int pizza_up13_cost = 170.000.000.000.000;
-        public int pizza_up14_cost = 2.100.000.000.000.000;
+
+        public long pizza_up1_cost = 15;
+        public long pizza_up2_cost = 100;
+        public long pizza_up3_cost = 1100;
+        public long pizza_up4_cost = 12000;
+        public long pizza_up5_cost = 130000;
+        public long pizza_up6_cost = 1400000;
+        public long pizza_up7_cost = 20000000;
+        public long pizza_up8_cost = 330000000;
+        public long pizza_up9_cost = 510000000;
+        public long pizza_up10_cost = 75000000000;
+        public long pizza_up11_cost = 1000000000000;
+        public long pizza_up12_cost = 14000000000000;
+        public long pizza_up13_cost = 170000000000000;
+        public long pizza_up14_cost = 2100000000000000;
 
 
         // Timer value
         public TimeSpan timeLeft;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -56,6 +59,29 @@ namespace Pizza_Clicker_WhoaGeilEy
         {
             pizza_count += pizza_cpt; // cpt wird hinzugegeben 
             lblPizza_count.Text = "Pizzen: " + pizza_count;
+        }
+
+
+        private void shop_up2_Click_1(object sender, EventArgs e)
+        {
+            if (pizza_count >= pizza_up2_cost)
+            {
+                pizza_cpt += 1;
+                pizza_count -= pizza_up2_cost;
+                pizza_up2_cost = Convert.ToInt64(Convert.ToDouble(pizza_up2_cost) * 1.15);
+                lblPizza_count.Text = "Pizzen: " + pizza_count;
+            }
+        }
+
+        private void shop_up3_Click_1(object sender, EventArgs e)
+        {
+            if (pizza_count >= pizza_up3_cost)
+            {
+                pizza_cpt += 8;
+                pizza_count -= pizza_up3_cost;
+                pizza_up3_cost = Convert.ToInt64(Convert.ToDouble(pizza_up3_cost) * 1.15);
+                lblPizza_count.Text = "Pizzen: " + pizza_count;
+            }
         }
     }
 }
